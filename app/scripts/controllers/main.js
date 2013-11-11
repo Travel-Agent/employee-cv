58df9fe5afb76edafacebdc48bbbeb9dad78b86b
Gilad Peleg <giladp007@gmail.com>
2013-11-11 22:06:28 +0200

    minor

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,12 +2,5 @@
 
 angular.module('employeeCvApp')
     .controller('MainCtrl', function ($scope, $http) {
-        $http.get('/api/awesomeThings')
-            .success(function (awesomeThings) {
-                console.log(awesomeThings);
-                $scope.awesomeThings = awesomeThings;
-            })
-            .error(function (err) {
-                console.log(err);
-            })
+
     });
